f511832c1e3e74c57676998254a06a2b9808b8df
Sebitax <sebastianvelasquez456@gmail.com>
2023-10-04 11:56:13 -0500

    Refactor: cambia los nombres de las vistas agregando el sufijo _view

--- Papiro_Tinta/main/urls.py
+++ Papiro_Tinta/main/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('index', views.index_view, name='index'),
     path('index.html', RedirectView.as_view(url='/index', permanent=True)),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
 ]
