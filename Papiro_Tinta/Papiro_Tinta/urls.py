45e7d3c74c7a99e9aa5ca248c3696b2215f1c71f
Sebitax <sebastianvelasquez456@gmail.com>
2023-10-04 11:31:02 -0500

    Feat: redirige desde direccion.com/index.html a direccion.com/index

--- Papiro_Tinta/Papiro_Tinta/urls.py
+++ Papiro_Tinta/Papiro_Tinta/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 from main import views
 
 urlpatterns = [
-    path('', include('main.urls')),
+    path('', RedirectView.as_view(url='index.html', permanent=False), name='index'),
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
 ]
--- Papiro_Tinta/main/urls.py
+++ Papiro_Tinta/main/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
+from django.views.generic import RedirectView
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main'),
+    path('index', views.index, name='index'),
+    path('index.html', RedirectView.as_view(url='/index', permanent=True)),
     path('login/', views.login, name='login'),
 ]
