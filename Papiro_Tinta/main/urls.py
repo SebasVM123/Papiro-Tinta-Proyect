c0c5ffebf032ba12eb5d673c5b5f95ace831a623
Sebitax <sebastianvelasquez456@gmail.com>
2023-10-07 17:20:11 -0500

    Feat: agrega el prefijo _url al nombre de las rutas y quita la redirección a index desde la pag principal

--- Papiro_Tinta/main/urls.py
+++ Papiro_Tinta/main/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('index', views.index_view, name='index'),
-    path('index.html', RedirectView.as_view(url='/index', permanent=True)),
-    path('login/', views.login_view, name='login'),
+    path('', views.index_view, name='index'),
+    path('login/', views.login_view, name='login_url'),
 ]
