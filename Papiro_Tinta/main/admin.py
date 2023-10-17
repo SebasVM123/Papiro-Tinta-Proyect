f546775d48c548aeaafa9228b2d4eca691d9a734
Sebitax <sebastianvelasquez456@gmail.com>
2023-10-17 14:28:39 -0500

    Feat: registra los modelos en admin.py

--- Papiro_Tinta/main/admin.py
+++ Papiro_Tinta/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(cliente)
+admin.site.register(tarjeta)
+admin.site.register(categoria)
+admin.site.register(cliente_tarjeta)
+admin.site.register(cliente_categoria)
