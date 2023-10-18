91c72d1f6b61a4433a87c664afc5f27933281bc8
felipe osorio <felipe.osorio12@utp.edu.co>
2023-10-18 11:13:45 -0500

    "registro completo"

--- Papiro_Tinta/main/urls.py
+++ Papiro_Tinta/main/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.views.generic import RedirectView
 
 from . import views
+#from .views import crear_registro
 
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('iniciar-sesion/', views.login_view, name='login_url'),
-    path('registrarse/', views.register_view, name='register_url')
+    path('registrarse/', views.register_view, name='register_url'),
+    path('crear-registro/', views.crear_registro, name='crear_registro')
 ]
--- Papiro_Tinta/main/views.py
+++ Papiro_Tinta/main/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render, redirect
+#from .forms import MiModeloForm
+from django.shortcuts import render, redirect
+from .forms import MiModeloForm
+from .models import cliente  # Asegúrate de importar el modelo
+from django.utils import timezone
+
+
 
 # Create your views here.
 def index_view(request):
@@ -9,3 +16,100 @@ def login_view(request):
 
 def register_view(request):
     return render(request, 'main/register.html')
+
+def crear_registro(request):
+    if request.method == 'POST':
+        form = MiModeloForm(request.POST)
+        if form.is_valid():
+            nuevo_cliente = form.save(commit=False)  # No guardes todavía
+            nuevo_cliente.fecha_registro = timezone.now()  # Establece la fecha de registro
+            nuevo_cliente.save()  # Ahora guarda la instancia
+            return redirect('index')
+    else:
+        form = MiModeloForm()
+    return render(request, 'main/register.html', {'form': form})
+
+"""
+def crear_registro(request):
+    if request.method == 'POST':
+        form = MiModeloForm(request.POST)
+        if form.is_valid():
+            nuevo_cliente = form.save()  # Esto guarda el nuevo cliente en la base de datos
+            # Redirige al usuario a la página de éxito o a cualquier otra página que desees.
+            return redirect('index')
+    else:
+        form = MiModeloForm()
+
+    return render(request, 'main/register.html', {'form': form})
+
+"""
+
+"""
+def crear_registro(request):
+    if request.method == 'POST':
+        form = MiModeloForm(request.POST)
+        if form.is_valid():
+            # Crear una instancia del modelo y completarla con los datos del formulario
+            nuevo_cliente = cliente(
+                nombre=form.cleaned_data['nombre'],
+                apellido=form.cleaned_data['apellido'],
+                contraseña=form.cleaned_data['contraseña'],
+                fecha_nacimiento=form.cleaned_data['fecha_nacimiento'],
+                direccion=form.cleaned_data['direccion'],
+                ciudad=form.cleaned_data['ciudad'],
+                provincia=form.cleaned_data['provincia'],
+                pais=form.cleaned_data['pais'],
+                email=form.cleaned_data['email']
+            )
+            nuevo_cliente.save()  # Guarda el nuevo cliente en la base de datos
+
+            # Redirige al usuario a la página de éxito o a cualquier otra página que desees.
+            return redirect('index')
+    else:
+        form = MiModeloForm()
+        """
+
+"""
+def crear_registro(request):
+    if request.method == 'POST':
+        form = MiModeloForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirige al usuario a la página de éxito o a cualquier otra página que desees.
+            return redirect('index')
+    else:
+        form = MiModeloForm()
+        
+    return render(request, 'register.html', {'form': form})
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def crear_registro(request):
+    if request.method == 'POST':
+        form = MiModeloForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+        
+    else:
+        form = MiModeloForm()
+        
+    return render(request, 'main/templates/register.html', {'form': form})
+            
+"""
