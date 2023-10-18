8f281be1ea295ddb73650ce6fe7cff5a0f0ccf40
Marcus <e.galvis@utp.edu.co>
2023-10-18 08:23:08 -0500

    "inicio de sesion v2"

--- Papiro_Tinta/main/views.py
+++ Papiro_Tinta/main/views.py
@@ -7,7 +7,20 @@ def index_view(request):
     return render(request, 'main/index.html')
 
 def login_view(request):
-    return render(request, 'main/login.html')
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('index')
+        else:
+            messages.success(request, ('No se pudo iniciar sesion, por favor vuelve a intentar'))
+            return redirect('login_url')
+            # Return an 'invalid login' error message.
+    else:
+        return render(request, 'main/login.html')
 
 def register_view(request):
     return render(request, 'main/register.html')
