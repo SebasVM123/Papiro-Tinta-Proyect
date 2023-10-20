8cf11491b3c5710d7a681ad0edd67cde59e8f1b9
Marcus <e.galvis@utp.edu.co>
2023-10-20 08:05:02 -0500

    "inicio de sesion solo con correo, version entregable sprint#1"

--- Papiro_Tinta/Papiro_Tinta/settings.py
+++ Papiro_Tinta/Papiro_Tinta/settings.py
@@ -130,3 +130,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'main.backends.EmailBackend',  # Reemplaza 'tu_app' con el nombre de tu app
+    'django.contrib.auth.backends.ModelBackend',
+]
