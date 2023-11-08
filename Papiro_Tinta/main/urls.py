e8d41abc05da069a04e2707190e2d51db553eb4f
Sebitax <sebastianvelasquez456@gmail.com>
2023-11-08 16:00:39 -0500

    Refactor: elimina todo el código del back para refactorizarlo

--- Papiro_Tinta/main/urls.py
+++ Papiro_Tinta/main/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.contrib import admin
-from .views import custom_admin_logout
 
 from . import views
+
+from . import views2
 #from .views import crear_registro
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('iniciar-sesion/', include('django.contrib.auth.urls')),
-    path('iniciar-sesion/', views.login_view, name='login_url'),
-    path('registrarse/', views.register_view, name='register_url'),
-    path('crear-registro/', views.crear_registro, name='crear_registro'),
-    path('home/', views.home, name='home'),
-    path('logout/', views.logout_view, name='logout_url'),
-    path('admin/logout/', custom_admin_logout, name='custom_admin_logout'),
-    path('admin/', admin.site.urls),
-    path('home/perfil', views.client_profile_view, name='client_profile_url')
+    path('iniciar-sesion', views.login_view, name='login_url'),
+    path('registrarse', views.register_view, name='register_url'),
 ]
